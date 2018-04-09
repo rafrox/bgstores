c39508ad656cdad114a20557acfd01f0d12bc562
rafrox <37214120+rafrox@users.noreply.github.com>
2018-04-09 19:09:50 +1000

    up

--- books/spiders/__init__.py
+++ books/spiders/__init__.py
@@ -2,3 +2,19 @@
 #
 # Please refer to the documentation for information on how to create and manage
 # your spiders.
+class MySpider(CrawlSpider):
+    name = "advent"
+    allowed_domains = ["www.adventgames.com.au/"]
+    start_urls = [u'http://www.adventgames.com.au/c/4504822/1/all-games-a---k.html',
+                  {u'url': u'http://www.adventgames.com.au/Listing/Category/?categoryId=4504822&page=[1-5]',
+                   u'fragments': [{u'valid': True,
+                                   u'type': u'fixed',
+                                   u'value': u'http://www.adventgames.com.au/Listing/Category/?categoryId=4504822&page='},
+                                  {u'valid': True,
+                                   u'type': u'range',
+                                   u'value': u'1-5'}],
+                   u'type': u'generated'}]
+
+    def __init__(self, *a, **kw):
+        super(MySpider, self).__init__(*a, **kw)
+        dispatcher.connect(self.spider_closed, signals.spider_closed)
